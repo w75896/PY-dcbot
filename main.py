4f71fa47efe7e7d38ed4e05fb779d6a2b6665627
w75896 <85177690+w75896@users.noreply.github.com>
2022-06-18 18:08:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+from discord import channel
+import discord
+from discord.ext import commands
+import time as t 
 
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='$',intents=intents)
